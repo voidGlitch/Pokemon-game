e311e517f4205979b07f0e891a04ec7ef8be62fa
voidGlitch <official.shreshthav@gmail.com>
2022-03-26 18:03:03 +0530

    added collision boundaries on map

--- src/index.js
+++ src/index.js
@@ -7,11 +7,68 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-console.log(c);
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+// Step -1 Now its time we comment out these code that we use to seperate our canvas from the background
+// c.fillStyle = "white";
+// c.fillRect(0, 0, canvas.width, canvas.height);
 
+//Step-2 array of 40 array with 70 items each
+const collisionMap = [];
+
+//we use 70 because our canvas image has 70 pixels by 40 pixel
+for (let i = 0; i < collision.length; i += 70) {
+  //but we dont want to show only the first 70 element 70 times
+  // console.log(collision.slice(0, 70));
+  //what we done here is firse we console the 70 items with 70 array so we increment the 70 for the next 70 array items and so on
+  // console.log(collision.slice(i, i + 70));
+  //next one slice(70,140) and keeps increment by 70
+  collisionMap.push(collision.slice(i, 70 + i));
+}
+console.log(collisionMap);
 //create image within the javascript
+
+//Now we created a class which will represent our rectangle tiles in red colour
+class Boundary {
+  static width = 90;
+  static height = 90;
+  constructor({ position }) {
+    this.position = position;
+    // We have zoomed our town image by 750 so the tiles of the collision is also zoomed so 12(before zoomed 12*12)*7.5=90
+    this.width = 90;
+    this.height = 90;
+  }
+  //Step 3 now we used to draw the square object
+  draw() {
+    // NOW we used fill color red of canvas context and define the position of x and y
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const offset = {
+  x: -870,
+  y: -1500,
+};
+const boundaries = [];
+//Step 4 so now we need to sign the boundaries precisely on map based off this collision map where our 1025 is the array
+collisionMap.forEach((row, i) => {
+  //Fistly we iterated our row i.e 40 arrays then we iterate over each 40 array so that we can get each particular 1025 symbol in this i is position of row and j is the position number of item inside the array
+  row.forEach((symbol, j) => {
+    // Now wherever we see the symbol 1025 we set the value position
+    if (symbol === 1025) {
+      boundaries.push(
+        new Boundary({
+          position: {
+            // j is the value where the symbol is diagonally then multiply it by 90 which gives us the exact position of the boundary on x axis same as y position but on horizontal
+            //we write offset.x that gonna subtact the width and the height so the position is centralized
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+    }
+  });
+});
+console.log(boundaries);
 
 const image = new Image();
 image.src = "../Pokemon Assets/PalletTown1.png";
@@ -36,11 +93,12 @@ class Sprite {
     c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
+
 //defining constructor
 const background = new Sprite({
   position: {
-    x: -500,
-    y: -1000,
+    x: offset.x,
+    y: offset.y,
   },
   image: image,
 });
@@ -84,7 +142,11 @@ function animate() {
   //The window.requestAnimationFrame() method tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an animation before the next repaint. The method takes a callback as an argument to be invoked before the repaint.
   window.requestAnimationFrame(animate);
   console.log(animate);
+  // Step 6 Now we draw our collision tiles on the map
   background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
   c.drawImage(
     playerImage,
     0,
